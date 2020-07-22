11f8576c5b07bbfc25452147febf97d0fbb28bcb
ercenk <ercenk@ercenk.com>
2020-07-22 12:48:38 -0700

    sync

---
+++ assignManagedAppRBAC.sh
@@ -0,0 +1,5 @@
+az login --identity
+az identity list 
+
+# run steps 2 and 3 to get managed app ID
+az role assignment create --role "Reader" --assignee-object-id "<principal ID for the managed identity>" --scope "managed app resource ID from steps 2 and 3"
